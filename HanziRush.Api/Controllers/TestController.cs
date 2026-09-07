d0bd2adf36aa4b5311a61e83d093b1ed7f6ed416
duongthuan0405 <duongthuan0405@gmail.com>
2026-09-07 21:58:20 +0700

    REF: with warning  async method lacks 'await' operators

--- HanziRush.Api/Controllers/TestController.cs
+++ HanziRush.Api/Controllers/TestController.cs
@@ -2,11 +2,26 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HanziRush.Api.Controllers;
 
+public class ResultDTO<T>
+{
+    public bool IsSuccess {get; set;} = default;
+    public string Message {get; set;} = string.Empty;
+    public T? Data {get; set; } = default(T);
+
+}
+
 public class TestController : ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<string>> TestCI()
+    public Task<ActionResult<ResultDTO<string>>> TestCI()
     {
-        return StatusCode(StatusCodes.Status200OK, "Test OK");
+
+        var res = new ResultDTO<string>()
+        {
+            IsSuccess = true,
+            Message = "Test CI OK",
+            Data = "Ok!"
+        };
+        return Task.FromResult<ActionResult<ResultDTO<string>>>(Ok(res));
     }
 }
